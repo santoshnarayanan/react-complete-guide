fa7814d70b276033c692fa224e4cf09e76a2da8a
santosh33 <santosh33@gmail.com>
2022-04-22 12:23:22 +0530

    correction of title displayed and concept of key introduced

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,7 +15,8 @@ const Expenses = (props) => {
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                {props.items.map(expense => <ExpenseItem title={expense.title} amount={expense.amount}
+                {props.items.map(expense => <ExpenseItem key={expense.id}
+                    title={expense.title} amount={expense.amount}
                                                          date={expense.date}/>)}
             </Card>
         </div>
